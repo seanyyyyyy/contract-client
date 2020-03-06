72543e139829e14a6602b85454a61758b9e87f7a
sean.liaw <sean.liaw@afterpaytouch.com>
2020-03-06 17:05:14 +1100

    updated stubs

--- src/test/java/hello/ContractRestClientApplicationTest.java
+++ src/test/java/hello/ContractRestClientApplicationTest.java
@@ -1,35 +1,33 @@
 package hello;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.junit.StubRunnerExtension;
 import org.springframework.cloud.contract.stubrunner.junit.StubRunnerRule;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//@AutoConfigureWireMock(stubs="IdeaProjects\u2069 ▸ \u2068spring-contract-base-server\u2069 ▸ \u2068build\u2069")
+
 public class ContractRestClientApplicationTest {
 
-    /*
     @Rule
     public StubRunnerRule stubRunnerRule = new StubRunnerRule()
             .downloadStub("com.example", "contract-server", "0.0.1-SNAPSHOT", "stubs")
             .withPort(8100)
             .stubsMode(StubRunnerProperties.StubsMode.LOCAL);
-    */
-    @RegisterExtension
-    public StubRunnerExtension stubRunnerExtension = new StubRunnerExtension()
-            .downloadStub("com.example:contract-server")
-            .repoRoot("stubs://file://seanl/IdeaProjects/spring-contract-base-server/build/stubs/")
-            .stubsMode(StubRunnerProperties.StubsMode.REMOTE);
 
     @Test
     public void get_person_from_service_contract() {
@@ -42,8 +40,8 @@ public class ContractRestClientApplicationTest {
         // then:
         BDDAssertions.then(personResponseEntity.getStatusCodeValue()).isEqualTo(200);
         BDDAssertions.then(personResponseEntity.getBody().getId()).isEqualTo(1l);
-        BDDAssertions.then(personResponseEntity.getBody().getName()).isEqualTo("foo");
-        BDDAssertions.then(personResponseEntity.getBody().getSurname()).isEqualTo("bee");
+        BDDAssertions.then(personResponseEntity.getBody().getName()).isEqualTo("foo-new");
+        BDDAssertions.then(personResponseEntity.getBody().getSurname()).isEqualTo("beer-new");
 
     }
 }
